118e2e4673684bb8462ea7a4ddf6f4889d63d343
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-07-29 16:19:08 -0400

    Create the TitleBar component.
    $ ng g c ss/title-bar --module=ss --prefix=ss --spec=false

--- src/app/ss/ss.module.ts
+++ src/app/ss/ss.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { ContentComponent } from './content/content.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FrameworkBodyComponent, ContentComponent],
-  exports: [
-    FrameworkBodyComponent, ContentComponent
-  ],
+  declarations: [FrameworkBodyComponent, ContentComponent, TitleBarComponent],
+  exports: [FrameworkBodyComponent, ContentComponent, TitleBarComponent],
 })
 export class SsModule { }
