be21f64964e15b28babdb4555a2ea5f0194d5783
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-07-29 16:05:14 -0400

    Add the framework-body component to ss, and add it to the app component html.
    $ ng g c ss/framework-body --module=ss --prefix=ss --spec=false

--- src/app/ss/ss.module.ts
+++ src/app/ss/ss.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [FrameworkBodyComponent],
+  exports: [
+    FrameworkBodyComponent,
+  ],
 })
 export class SsModule { }
