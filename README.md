fd7d44e6760c367775bb424f5a73df966eef8d51
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-07-30 00:36:14 -0400

    Making the Title Bar responsive.
    Install font-awesome (since Bootstrap 4 dropped glyphicons)
    
    $ npm install font-awesome --save
    
    See http://fontawesome.io/icons/

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This project is based on the Pluralsight course: Building a Responsive SPA Frame
 
 Most of this course was completed with Sutton's angular project: sscrm
 
-A big difference in this version is the use of bootstrap@4.0.0-alpha.6 (instead of v 3.7)
+BOOTSTRAP 4 WITH FONT-AWESOME
+A big difference in this version is the use of bootstrap@4.0.0-alpha.6 (instead of v 3.7).
+Since Bootstrap 4 drops glyphicons, font-awesome icons are used.
 
 ##TODO
 - Create npm package for the SPA Framework (see Pluralsight - NPM Playbook by Joe Eames)
--- src/app/ss/title-bar/title-bar.component.ts
+++ src/app/ss/title-bar/title-bar.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+import { ScreenService } from '../services/screen.service';
+// import { MenuService } from '../services/menu.service';
+
+/**
+ * Displays the Title Bare of the framework whic includes a logo.png and the configurable Top Bar.
+ *
+ * User CSS Styles allowed: user-title-bar
+ *
+ * @export
+ * @class TitleBarComponent
+ */
 @Component({
   selector: 'ss-title-bar',
   templateUrl: './title-bar.component.html',
   styleUrls: ['./title-bar.component.scss']
 })
-export class TitleBarComponent implements OnInit {
+export class TitleBarComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(
+    protected screenService: ScreenService,
+    // protected menuService: MenuService,
+  ) { }
 
 }
