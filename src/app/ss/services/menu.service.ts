aad94107e1c6d05d310a0ea20583130581f4a86a
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-07-31 09:39:34 -0400

    Menus and Navigation: Fixing Menu Issues

--- src/app/ss/services/menu.service.ts
+++ src/app/ss/services/menu.service.ts
@@ -5,11 +5,17 @@ import { MenuItem } from '../menus/menu-item/menu-item.model';
 @Injectable()
 export class MenuService {
 
+  /** Array of menu items */
   items: Array<MenuItem>;
+
+  /** Whether or not we display the vertical menu (and hide the horizontal menu) */
   isVertical = false;
-  showingLeftSideMenu = false;
+
+  /** Keeps track of whether or not we are showing the left-hand side vertical menu */
+  showingLeftSideMenu = false; // gets set to true when the left-side menu shows.
 
   toggleLeftSideMenu(): void {
+    this.isVertical = true; // Set isVertical to true since toggleLeftSideMenu() only gets called when *screenBelowLarge is true.
     this.showingLeftSideMenu = !this.showingLeftSideMenu;
   }
 
