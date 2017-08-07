6fe158feea8bdaa27644a28ddc8ca47624e84533
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-08-07 13:23:25 -0400

    79 - Building Responsive Content Panels and a Dashboard - Creating the Dashboard [1m 25s]

--- src/app/services/app-data.service.ts
+++ src/app/services/app-data.service.ts
@@ -13,7 +13,7 @@ export class AppDataService {
     { id: 1, name: 'Switzerland', epiIndex: 87.67 },
     { id: 2, name: 'Luxembourg', epiIndex: 83.29 },
     { id: 3, name: 'Australia', epiIndex: 82.4 },
-    { id: 4, name: 'Singaport', epiIndex: 81.78 },
+    { id: 4, name: 'Singapore', epiIndex: 81.78 },
     { id: 5, name: 'Czech Republic', epiIndex: 81.47 },
     { id: 6, name: 'Germany', epiIndex: 80.47 },
     { id: 7, name: 'Spain', epiIndex: 79.09 },
